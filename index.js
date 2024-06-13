36de469b721c45d45943f2062034fe59261c2bfc
AnilaMacha <anilamacha2000@gmail.com>
2024-06-12 22:35:17 -0500

    form-validation

--- index.js
+++
@@ -1,3 +0,0 @@
-const parent=React.createElement("div",{id:"parent"}, React.createElement("div",{id:"child"}, React.createElement("h1",{id:"heading"},"heading from react")));
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
